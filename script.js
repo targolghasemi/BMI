6786d887e03ab998d91d2687bc1d5949c08f964b
targolghasemi <targolghasemi85@gmail.com>
2025-09-09 15:36:01 +0330

    last commit

--- script.js
+++ script.js
@@ -5,13 +5,23 @@ form.addEventListener('submit', function(e) {
     const height = parseInt(document.querySelector('#height').value); 
     const weight = parseInt(document.querySelector('#weight').value); 
     const results = document.querySelector('#results');
+    const guide = document.querySelector('#weight-guide');
 
     if ((height < 0 ) || (isNaN(height))) {
-        results.innerHTML = "Please provide a vaild height";
-    } else if ( (weight < 0 ) || (isNaN(weight))) {
-        results.innerHTML = "Please provide a vaild weight";
+        results.innerHTML = "Please provide a valid height";
+    } else if ((weight < 0 ) || (isNaN(weight))) {
+        results.innerHTML = "Please provide a valid weight";
     } else {
-        const bmi = (weight / ((height*height)/10000)).toFixed(2);
-        results.innerHTML = `<span>${bmi}</span>`
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+
+        results.innerHTML = `<span>${bmi}</span>`;
+
+        if (bmi < 18.6) {
+            guide.innerHTML = "Under Weight";
+        } else if (bmi >= 18.6 && bmi <= 24.9) {
+            guide.innerHTML = "Normal Range";
+        } else {
+            guide.innerHTML = "Over Weight";
+        }
     }
 });
